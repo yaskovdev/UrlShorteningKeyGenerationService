71faacc70c0fa673adbcc63f05db93576f36f771
yaskovdev <yaskovdev@gmail.com>
2021-08-01 00:00:19 +0300

    Avoid a situation when two clients get the same keys, assuming there will be only one instance of the service (another instance is a standby replica).

--- UrlShorteningKeyGenerationService/Services/KeyGenerationService.cs
+++ UrlShorteningKeyGenerationService/Services/KeyGenerationService.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UrlShorteningKeyGenerationService.Services
 {
     public class KeyGenerationService : IKeyGenerationService
     {
+        private static readonly SemaphoreSlim Semaphore = new(1, 1);
+
         private readonly ICosmosDbService cosmosDbService;
 
         public KeyGenerationService(ICosmosDbService cosmosDbService)
@@ -16,12 +19,25 @@ namespace UrlShorteningKeyGenerationService.Services
 
         public async Task<IEnumerable<string>> TakeKeys(int limit)
         {
+            await Semaphore.WaitAsync();
+            try
+            {
+                var keys = await GetUrlKeys(limit);
+                await cosmosDbService.MarkUrlKeysAsTaken(keys);
+                return keys;
+            }
+            finally
+            {
+                Semaphore.Release();
+            }
+        }
+
+        private async Task<IReadOnlyCollection<string>> GetUrlKeys(int limit)
+        {
             var entities = await cosmosDbService.GetUrlKeysAsync(limit);
-            var keys = entities
+            return entities
                 .Select(it => it.Id)
                 .ToImmutableList();
-            await cosmosDbService.MarkUrlKeysAsTaken(keys);
-            return keys;
         }
     }
 }
