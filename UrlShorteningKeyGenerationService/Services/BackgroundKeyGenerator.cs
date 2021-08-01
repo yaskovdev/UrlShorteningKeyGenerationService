5c10c51c8861099e1196e641ce1ba2c2d7805ebb
yaskovdev <yaskovdev@gmail.com>
2021-08-01 19:13:19 +0300

    Change interval.

--- UrlShorteningKeyGenerationService/Services/BackgroundKeyGenerator.cs
+++ UrlShorteningKeyGenerationService/Services/BackgroundKeyGenerator.cs
@@ -19,7 +19,7 @@ namespace UrlShorteningKeyGenerationService.Services
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            timer.Change(Zero, FromMilliseconds(500));
+            timer.Change(Zero, FromMilliseconds(5000));
             return CompletedTask;
         }
 
